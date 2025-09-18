7e2bd316f12ddc7f2bcbf88671049fa629f20321
MaripeddiSupraj <supraj.maripeddi@gmail.com>
2025-09-18 19:05:57 +0530

    Test PR workflow: Add comments and tags to AWS modules
    - Added descriptive comment to EC2 instance resource
    - Added Purpose tag to S3 bucket for better resource identification
    - Testing GitHub Actions workflow for PR automation

--- aws/modules/s3/main.tf
+++ aws/modules/s3/main.tf
@@ -1,9 +1,11 @@
+# S3 bucket with encryption and security configurations
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 
   tags = {
     Name        = var.bucket_name
     Environment = var.environment
+    Purpose     = "Application storage"
   }
 }
 
